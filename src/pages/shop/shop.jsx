040aee9dd84e476e351bff64ca6f0b0e8c3ccad4
Motoki Naito <motoki.developer@gmail.com>
2019-08-18 17:23:12 -0700

    moved this directory and shop data into its own reducer

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,23 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from "./shop.data.js";
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import CollectionPage from "../collection/collection";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-
-class ShopPage extends Component {
-  state = {
-    collections: SHOP_DATA
-  };
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
