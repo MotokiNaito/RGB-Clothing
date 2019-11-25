65a968c37f762213e38f1665cf48613e32721a45
Motoki Naito <motoki.developer@gmail.com>
2019-11-25 22:22:01 +0900

    update home page

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -18,8 +18,8 @@ const HomePage = () => (
       </MainTextContainer>
     </MainImageContainer>
     <IntroText>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-      type and scrambled it to make a type specimen book.
+      RGB Clothing is one of the largest international fashion companies. It belongs to Inditex, one of the world’s largest distribution groups. The customer is at the heart of our unique business
+      model, which includes design, production, distribution and sales through our extensive retail network.
     </IntroText>
     <HomePageContainer>
       <h2>Category</h2>
